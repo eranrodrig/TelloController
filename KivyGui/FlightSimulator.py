23ff1dcf648af54502440d89601e985ca366e979
Eran Rodrig <eran.rodrig@gmail.com>
2020-08-22 17:08:08 +0300

    emptied files for lab experiment

--- KivyGui/FlightSimulator.py
+++ KivyGui/FlightSimulator.py
@@ -28,10 +28,10 @@ class FlightSimulator(FloatLayout):
     wait = 0.2
 
     def takeoff(self):
-        tello.take_off()
+        pass
 
     def land(self):
-        tello.land()
+        pass
 
     def bind_joysticks(self):
         left_control = self._get_joystick(self.left_joystick)
@@ -40,16 +40,7 @@ class FlightSimulator(FloatLayout):
         debounce(self.wait, self._right_joystick_control, right_control)
 
     def _left_joystick_control(self, instance):
-        angle = instance.angle
-        if instance.magnitude > 0.9:
-            if angle > 315 or angle < 45:
-                tello.ccw()
-            elif 45 < angle < 135:
-                tello.up()
-            elif 135 < angle < 225:
-                tello.cw()
-            else:
-                tello.down()
+        pass
 
     def _get_joystick(self, parent):
         if isinstance(parent, Joystick):
@@ -60,16 +51,7 @@ class FlightSimulator(FloatLayout):
                     return child
 
     def _right_joystick_control(self, instance):
-        angle = instance.angle
-        if instance.magnitude > 0.9:
-            if angle > 315 or angle < 45:
-                tello.left()
-            elif 45 < angle < 135:
-                tello.forward()
-            elif 135 < angle < 225:
-                tello.right()
-            else:
-                tello.backward()
+        pass
 
 
 class VideoScreen(Image):
@@ -80,13 +62,7 @@ class VideoScreen(Image):
         Clock.schedule_interval(self.update, 1.0 / self.fps)
 
     def update(self, dt):
-        frame = tello.take_picture()
-        img = frame.astype(np.uint8)
-        image_texture = Texture.create(
-            size=(img.shape[1], img.shape[0]))
-        image_texture.blit_buffer(img.tostring())
-        # display image from the texture
-        self.texture = image_texture
+        pass
 
 
 class FlightSimulatorApp(App):
